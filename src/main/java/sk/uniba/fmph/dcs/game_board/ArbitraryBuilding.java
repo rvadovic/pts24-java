86f11ccd576b5f385d82e8dbe5dc48947b0409df
Adam <adam.fremal@gmail.com>
2024-11-14 22:22:28 +0100

    ArbitraryBuilding and ArbitraryTest added

---
+++ src/main/java/sk/uniba/fmph/dcs/game_board/ArbitraryBuilding.java
@@ -0,0 +1,33 @@
+package sk.uniba.fmph.dcs.game_board;
+
+import sk.uniba.fmph.dcs.stone_age.Effect;
+
+import java.util.Collection;
+import java.util.OptionalInt;
+
+public final class ArbitraryBuilding implements Building {
+    private final int maxNumberOfResources;
+    private static final int MAX_NUMBER_ALLOWED = 7;
+
+    public ArbitraryBuilding(final int maxNumberOfResources) {
+        this.maxNumberOfResources = maxNumberOfResources;
+        if (maxNumberOfResources <= 0 || maxNumberOfResources > MAX_NUMBER_ALLOWED) {
+            throw new IllegalArgumentException("At least 1 resource and most 7 resources are required");
+        }
+    }
+
+    @Override
+    public OptionalInt build(final Collection<Effect> resources) {
+        if (resources.size() > maxNumberOfResources || resources.isEmpty()) {
+            return OptionalInt.empty();
+        }
+        int sum = 0;
+        for (Effect resource : resources) {
+            if (!resource.isResource()) {
+                return OptionalInt.empty();
+            }
+            sum += resource.points();
+        }
+        return OptionalInt.of(sum);
+    }
+}
